95ba7dc58274e4d91ac83de28edcaac690de90d6
AGUER20 <abraham.aguer@strathmore.edu>
2024-07-09 10:52:05 +0300

    dleleted the navbar

--- processes/project_processes.php
+++ processes/project_processes.php
@@ -5,12 +5,12 @@ require_once "../config/dbConnect.php";
 if (isset($_POST["submit"])) {
     $projectname = mysqli_real_escape_string($dbConn, $_POST["projectname"]);
     $description = mysqli_real_escape_string($dbConn, $_POST["description"]);
-    $started = mysqli_real_escape_string($dbConn, $_POST["started"]);
-    $completed = mysqli_real_escape_string($dbConn, $_POST["completed"]);
+    $price = mysqli_real_escape_string($dbConn, $_POST["price"]);
+
 
     $insertProject = "
-    INSERT INTO projects (projectname, description, started, completed) 
-    VALUES ('$projectname', '$description', '$started', '$completed')";
+    INSERT INTO projects (projectname, description, price) 
+    VALUES ('$projectname', '$description', '$price)";
 
     if ($dbConn->query($insertProject) === TRUE) {
         echo "New project record created successfully";
--- services.php
+++ services.php
@@ -8,10 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">ADEASE</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNavDropdown">
@@ -58,15 +55,11 @@
                 <span id="descriptionError" class="error"></span>
             </div>
             <div class="form-group">
-                <label for="started">Start Date</label>
-                <input type="date" id="started" name="started" required>
-                <span id="startedError" class="error"></span>
+                <label for="started">Price</label>
+                <input type="text" id="started" name="started" required>
+                <span id="priceError" class="error"></span>
             </div>
-            <div class="form-group">
-                <label for="completed">Completion Date</label>
-                <input type="date" id="completed" name="completed" required>
-                <span id="completedError" class="error"></span>
-            </div>
+        
             <button type="submit" name="submit">Submit</button>
         </form>
     </div>
